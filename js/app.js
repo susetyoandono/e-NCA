92b9afa550262c58a397b296d849ff5998433597
susetyoandono <susetyo.andono@gmail.com>
2026-09-12 16:09:52 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,18 +10,14 @@ async function testSupabaseConnection() {
 
     try {
 
-        const { error } = await supabase
-            .from("system_test")
-            .select("*")
-            .limit(1);
+        const { data, error } = await supabase.auth.getSession();
 
-        // Untuk sementara table system_test belum ada.
-        // Error "relation does not exist" berarti connection
-        // ke Supabase sudah berhasil tetapi table belum dibuat.
-
-        if (error && !error.message.includes("does not exist")) {
+        if (error) {
             throw error;
         }
+
+        console.log("Supabase connected.");
+        console.log("Session:", data.session);
 
         statusElement.textContent =
             "Supabase connection: CONNECTED";
@@ -31,7 +27,7 @@ async function testSupabaseConnection() {
         console.error("Supabase connection error:", error);
 
         statusElement.textContent =
-            "Supabase connection: ERROR - Check console";
+            "Supabase connection: ERROR";
     }
 }
 
