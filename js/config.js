4463aa1a1e7c0deb351286af58eecff716d29761
susetyoandono <susetyo.andono@gmail.com>
2026-09-12 16:01:50 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@
 
 export const SUPABASE_URL = "https://dqmjdodxgwufeccqikzt.supabase.co";
 
-export const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE";
+export const SUPABASE_ANON_KEY = "sb_publishable_dSQ9AXLlCdqLcLNOMhcxyg_0unG-BYz";
