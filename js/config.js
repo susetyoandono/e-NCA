cdf24b94b18a103c37b706b81f35ab6a12a722e2
susetyoandono <susetyo.andono@gmail.com>
2026-09-12 16:21:50 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 // e-NCA - Supabase Configuration
 // =========================================================
 
-export const SUPABASE_URL = "dqmjdodxgwufeccqikzt";
+export const SUPABASE_URL = "https://dqmjdodxgwufeccqikzt.supabase.co";
 
 export const SUPABASE_ANON_KEY = "sb_publishable_dSQ9AXLlCdqLcLNOMhcxyg_0unG-BYz";
